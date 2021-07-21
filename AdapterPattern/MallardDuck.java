4ff0f1b3c21cde27c29edb0163e48cbad20b211b
Song <9901590@naver.com>
2021-07-21 23:58:58 +0900

    AdapterPattern

---
+++ AdapterPattern/MallardDuck.java
@@ -0,0 +1,11 @@
+package AdapterPattern;
+
+public class MallardDuck implements Duck {
+    public void quack(){
+        System.out.println("Quack");
+    }
+
+    public void fly(){
+        System.out.println("I'm flying");
+    }
+}
