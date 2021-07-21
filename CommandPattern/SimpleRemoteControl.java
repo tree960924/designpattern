b381a8be71a4f932e5c3e8bf973f5017d9340541
Song <9901590@naver.com>
2021-07-21 22:10:05 +0900

    CommandPattern

---
+++ CommandPattern/SimpleRemoteControl.java
@@ -0,0 +1,15 @@
+package CommandPattern;
+
+public class SimpleRemoteControl {
+    Command slot;
+
+    public SimpleRemoteControl(){}
+
+    public void setCommand(Command command){
+        this.slot = command;
+    }
+
+    public void buttonWasPressed(){
+        slot.execute();
+    }
+}
